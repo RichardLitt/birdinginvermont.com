70d63c609a1c9d20ed9ab7788eb004cd14b136e4
Richard Littauer <richard+github@burntfen.com>
2020-08-20 16:37:56 -0400

    Edited footer and added icons

--- src/Footer.js
+++ src/Footer.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 import './footer.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope, faCodeBranch, faBriefcase } from '@fortawesome/free-solid-svg-icons'
 
 class Footer extends Component {
   render() {
     return (
       <footer className="footer">
         <div className="container">
-          <span className="text-muted">Email me: <a href="mailto:richard@birdinginvermont.com">richard@birdinginvermont.com</a>. Or <a href="https://github.com/RichardLitt/birdinginvermont.com">contribute on GitHub</a>.</span>
+          <div className="row">
+            <div className="col-12">
+              <span className="text-muted">Mail <a href="mailto:richard@birdinginvermont.com"><FontAwesomeIcon icon={faEnvelope} /></a> Code <a href="https://github.com/RichardLitt/birdinginvermont.com"><FontAwesomeIcon icon={faCodeBranch} /></a> Terms <a href="/terms"><FontAwesomeIcon icon={faBriefcase} /></a></span>
+              </div>
+          </div>
         </div>
       </footer>
     )
