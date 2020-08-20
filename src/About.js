b336a3aef87866a99d8e0d5b1116b087c5c806bd
Richard Littauer <richard+github@burntfen.com>
2020-08-20 16:38:45 -0400

    Edit homepage

--- src/About.js
+++ src/About.js
@@ -1,24 +1,30 @@
 import React, { Component } from 'react'
+import aboutPath from './About.md'
+const ReactMarkdown = require('react-markdown')
 
 class About extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = { text: null }
+  }
+
+  componentWillMount() {
+    const path = aboutPath
+    fetch(path).then((response) => response.text()).then((text) => {
+      this.setState({ text: text })
+    })
+  }
+
   render() {
+
+
     return (
-      <div className="container-md">
+      <div id="about" className="container-md">
         <div className="row">
           <div className="col-md-8 col-sm-12 text-left">
             <img class="banner-image" src="rbgr.png" alt="Rose-breasted Grosbeak. © 2020 Richard Littauer"/>
-            <p style={{backgroundColor:"#FFFF00"}}><b>Notice:</b> This site is a work in construction. Eventually, we'll have a nice landing page.</p>
-            <p>For now, there are a few things we have here. First, <b>the map below</b> can be populated with <a href="https://ebird.org/downloadMyData">your eBird data</a> to show you what species you've seen in each town in Vermont. You can also upload your data regarding your photo or audio submissions to the Macaulay library. The <b>Biophysical Regions</b> tabs shows you what you've sene in each region of Vermont. Clicking and unclicking on a town once you've loaded your data allows you to move your mouse and look around some more.</p>
-            <p>The <b>Subspecies</b> page is in active development, too, but will eventually show you information on the birds in Vermont that have interesting subspecies. For now, they'll likely be updated frequently one at a time.</p>
-            <p>The <b>NFCs</b> page is for information related to Nocturnal Flight Calls, and species research there.</p>
-            <p>The <b>Rarities</b> page will check your eBird records from 2020 for rare birds that should be submitted to the Vermont Birds Records Committee.</p>
-            <h2>Updates</h2>
-            <ul>
-              <li><b>August 18th:</b> Added the <a href="/rarities">VBRC Records checker</a>.</li>
-              <li><b>August 18th:</b> Added the <a href="/subspecies/wbnu">White-breasted Nuthatch</a> subspecies page.</li>
-              <li><b>August 19th:</b> Added the <a href="/nfc-species/cawa">Canada Warbler</a> NFC page.</li>
-              <li><b>August 20th:</b> Added the <a href="/nfc-species/upsa">Upland Sandpiper</a> NFC page.</li>
-            </ul>
+            <ReactMarkdown source={this.state.text} escapeHtml={false} />
           </div>
         </div>
       </div>
---
+++ src/About.md
@@ -0,0 +1,24 @@
+<div class="tagline">
+Combining technology and research to make birding more rewarding.
+</div>
+
+Welcome to Birding in Vermont! This site is dedicated to helping birders by providing tools and resources on how to bird, better.
+
+- **Vermont Towns**: Upload your eBird data to see what towns you've birded in, and what birds you have or haven't seen there. As well, you can look at what birds you've seen in Vermont's biophysical regions.
+- **Nocturnal Flight Calls (NFCs)**: We're building a database of pages about specific species of birds identifiable by their nocturnal calls that they give in migration. Our goal here is to be a definitive resource for helping NFC enthusiasts learn more about how to identify birds and approach NFC research.
+- **Subspecies**: Vermont has over a hundred birds which are identifiable down to a specific subspecies. Identifying down to the subspecies level takes work, knowledge, and patience. We're making that easier by providing easy guides to learn how to look at birds closer.
+- **VBRC Rarities**: The Vermont Birds Record Committee has a list of birds which they want records for, each year, so that they know what birds are in the state. This tool checks your eBird data for any observations which you should report to VBRC, so you don't have to struggle to find all of your observations.
+
+More pages - specifically in the NFC and Subspecies sections - are in the works or on the way. And there are more tools we want to develop!
+
+This site is very much a **work in progress**. If you have any thoughts, comments, suggestions, or if you saw a cool bird and want to tell someone, get in touch! For now, all of this work is done by [Richard Littauer](https://ebird.org/profile/Mjg0MTUx/US-VT), in combination with help from dozens of other birders.
+
+Get in touch by emailing [richard@birdinginvermont.com](mailto:richard@birdinginvermont.com). Alternatively, swing by Montpelier, and let's go (socially-distantly) birding.
+
+### Updates
+
+- **August 18th:** Added the [VBRC Records checker](/rarities).
+- **August 18th:** Added the [White-breasted Nuthatch](/subspecies/wbnu) subspecies page.
+- **August 19th:** Added the [Canada Warbler](/nfc-species/cawa) NFC page.
+- **August 20th:** Added the [Upland Sandpiper](/nfc-species/upsa) NFC page.
+- **August 20th:** Added a [Terms](/terms) page.
