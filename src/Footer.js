55a1ece1571af9e897e416ebd70357cab78ce180
Ben Acker <benacker@gmail.com>
2021-01-22 17:17:29 -0500

    temp add netlify identity

--- src/Footer.js
+++ src/Footer.js
@@ -10,7 +10,7 @@ class Footer extends Component {
         <div className="container">
           <div className="row">
             <div className="col-12">
-              <span className="text-muted">Mail <a href="mailto:richard@birdinginvermont.com"><FontAwesomeIcon icon={faEnvelope} /></a> Code <a href="https://github.com/RichardLitt/birdinginvermont.com"><FontAwesomeIcon icon={faCodeBranch} /></a> Terms <a href="/terms"><FontAwesomeIcon icon={faBriefcase} /></a></span>
+              <span className="text-muted">Mail <a href="mailto:richard@birdinginvermont.com"><FontAwesomeIcon icon={faEnvelope} /></a> Code <a href="https://github.com/RichardLitt/birdinginvermont.com"><FontAwesomeIcon icon={faCodeBranch} /></a> Terms <a href="/terms"><FontAwesomeIcon icon={faBriefcase} /></a><a data-netlify-identity-button>Login</a></span>
               </div>
           </div>
         </div>
