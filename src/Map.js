0ebbed229bd064b55773a8d5ea6ffaa8ca489a90
Richard Littauer <richard+github@burntfen.com>
2021-02-25 12:48:31 -0500

    Reword

--- src/Map.js
+++ src/Map.js
@@ -389,11 +389,11 @@ I will update this map every week, to show what new towns should be added. Note:
           {this.props.location.pathname === '/2100' && <div>
             <h1>Project 2100</h1>
             <p>This year, I am attempting to see 150 species in each and every county in Vermont - 2100 birds in total.</p>
-            <p>The far more experienced Fred Pratt and Craig Provost have shown it is possible to do this over years of effort. I wonder if it is possible to do it on a shorter timeline, using eBird data, code, nocturnal flight calls, and help on the ground to make searching as efficient as possible.</p>
+            <p>The far more experienced birders Fred Pratt and Craig Provost have shown it is possible to do this over years of effort. I wonder if it is possible to do it on a shorter timeline, using eBird data, code, nocturnal flight calls, and help on the ground. As far as I know, this is a ridiculous enough goal that it has never been attempted.</p>
 <p>Obviously, this is quite difficult, and involves a lot of weekends driving around looking for specific birds. Below is a map of what I've seen so far, and where. I would love help with this project: tips on where to find good birds, driveways I can sleep in my car in on Saturday nights, and, mainly, partners to go birding with! I can't hear Golden-crowned Kinglets or Blackpoll Warblers, so people with good ears would be especially great to go birding with. Get in touch.</p>
 <p>Suggestions on how to make this page more useful are most welcome.</p>
 <p>This is not a solo effort. Friends who have helped so far: <a href="https://ebird.org/profile/NDIwNDA1/US-VT">Zac Cota</a>, <a href="https://ebird.org/vt/profile/MTgxNDYz/US-VT">Nathaniel Sharp</a>, and <a href="https://ebird.org/profile/NDM2MDU1/US-VT">Cedar Stanistreet</a>.</p>
-<p>Total county tick count so far: {this.props.data.vt2100data.map(x => x.speciesTotal).reduce((a, b) => a + b, 0)}.</p>
+<p>Total county ticks count so far: {this.props.data.vt2100data.map(x => x.speciesTotal).reduce((a, b) => a + b, 0)}.</p>
 <p>Last updated: <i>February 25, 2021</i>.</p>
           </div>}
           <div id="map" className="col-sm">
